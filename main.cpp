be9a40e5027067dbe339a62c9e6a6c45923b4d2d
WiktorK02 <123249470+WiktorK02@users.noreply.github.com>
2023-04-08 13:20:08 +0200

    Added counter text, fixed active circles color

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ public:
     void render(sf::RenderWindow& window, bool hasLineConnection) {
         circle.setPosition(pos);
         circle.setRadius(50);
-        if (hasLineConnection) {
+        if (!hasLineConnection) {
             circle.setFillColor(sf::Color::Green); // Change the color to green when circle is connected to a line
         }
         else {
@@ -58,15 +58,24 @@ obj::~obj()
 
 obj::obj()
 {
-    hasLineConnection = false; // initialize flag to false
+
 }
 
+int factorial(int n) {
+    int i, fact = 1;
+    i = n;
+    while (i >= 1) {
+        fact = fact * i;
+        i--;
+    }
+    return fact;
+}
 
 // Function to display the array
-void display(int a[], int n, obj circle[], sf::RenderWindow& window, sf::Text& percentage, float counter)
+void display(int a[], int n, obj circle[], sf::RenderWindow& window, sf::Text& percentage, sf::Text& counter_text, float counter)
 {
     double distnace_between = 0;
-    window.clear(sf::Color::Black); // Clear window with black color
+    window.clear(sf::Color::Black);
     for (int i = 0; i < n; i++) {
         // Draw lines between circles
         if (a[i] != -1) {
@@ -81,27 +90,40 @@ void display(int a[], int n, obj circle[], sf::RenderWindow& window, sf::Text& p
             line[1].color = sf::Color::Blue;
 
             // Check if circle is connected to a line
-            circle[i].render(window, true);
-            circle[a[i]].render(window, false);
+
+            //cout << a[i] << i << endl;
+
+            if (a[i] != i) {
+                circle[i].render(window, false);
+                circle[i].setLineConnection(false);
+            }
+            else {
+                circle[i].render(window, true);
+                circle[i].setLineConnection(false);
+            }
+
             window.draw(line);
         }
-        if (!distnace_between == 0) cout << distnace_between << endl;
+       
     }
-    cout << endl;
+    //cout << distnace_between << endl;
+    
+    // Draw text counter
+    counter_text.setString("Counter: " + to_string((int)counter));
+    window.draw(counter_text);
 
-    // Draw counter
-    counter = counter / 720 * 100;
+    counter = counter / factorial(n) * 100;
     std::ostringstream oss;
     oss << std::fixed << std::setprecision(0) << counter;
     std::string textStr = oss.str();
-    percentage.setString("Counter: " + textStr + "%");
+    percentage.setString("Percentage: " + textStr + "%");
     window.draw(percentage);
 
     window.display(); // Display the drawn frame
 }
 
 // Function to find the permutations
-void findPermutations(int a[], int n, obj circle[], sf::RenderWindow& window, sf::Text& percentage)
+void findPermutations(int a[], int n, obj circle[], sf::RenderWindow& window, sf::Text& percentage, sf::Text& counter_text)
 {
     int counter = 0;
     // Sort the given array
@@ -110,10 +132,10 @@ void findPermutations(int a[], int n, obj circle[], sf::RenderWindow& window, sf
     // Find all possible permutations
     do {
         counter++;
-        display(a, n, circle, window, percentage, counter);
+        display(a, n, circle, window, percentage, counter_text, counter);
 
         // Pause for a short duration
-        this_thread::sleep_for(chrono::milliseconds(200));
+        this_thread::sleep_for(chrono::milliseconds(20));
 
     } while (next_permutation(a, a + n));
 }
@@ -122,21 +144,33 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), ".");
     window.setFramerateLimit(120);
-
+    
+    //percentage text
     sf::Text percentage;
     sf::Font font;
     font.loadFromFile("minecraft_font.ttf");
     percentage.setFont(font); // font is a sf::Font
 
     // set the character size
-    percentage.setCharacterSize(24); // in pixels, not points!
+    percentage.setCharacterSize(20); // in pixels, not points!
 
     // set the color
     percentage.setFillColor(sf::Color::White);
+
+    //counter text
+    sf::Text counter_text;
+    counter_text.setFont(font); // font is a sf::Font
+
+    // set the character size
+    counter_text.setCharacterSize(20); // in pixels, not points!
+
+    // set the color
+    counter_text.setFillColor(sf::Color::White);
+    counter_text.setPosition(0, 25);
+    
     int counter = 0;
 
     obj circle[6];
-    //object possition 
     circle[0].set_pos(100, 100);
     circle[1].set_pos(1000, 50);
     circle[2].set_pos(500, 900);
@@ -161,17 +195,18 @@ int main()
                 break;
             }
         }
-        percentage.setString(to_string(counter));
-        counter++;
         
-        int a[] = { 0, 1, 2, 3, 4, 5}; // Indices of circles
+        int a[6] = { 0, 1, 2, 3, 4, 5}; // Indices of circles
         int n = sizeof(a) / sizeof(a[0]);
         
+        counter++;
+        
         window.clear();
-        findPermutations(a, n, circle, window, percentage);
+        findPermutations(a, n, circle, window, percentage, counter_text);
         window.display();
     }
 
     return 0;
 }
+
 
